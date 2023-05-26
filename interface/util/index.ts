8fea92fee24a991f50e662e14ecbba1cdf58de11
NightStar <57443207+Night-Star04@users.noreply.github.com>
2023-05-26 12:13:23 +0800

    repair: Fix function missing and wrong

--- interface/util/index.ts
+++ interface/util/index.ts
@@ -1,3 +1,5 @@
+import crypto from "crypto";
+
 function getQuery(req: Request): { [key: string]: string } {
   const { searchParams } = new URL(req.url);
   const query: { [key: string]: string } = {};
@@ -25,4 +27,20 @@ function UUID(len?: number): string {
   return crypto.randomUUID();
 }
 
-export { getQuery, UUID };
+async function fetcher_json<T = any>(url: string): Promise<T> {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then(async (res) => {
+        try {
+          if (!res.ok || res.status >= 400 || res.status < 200)
+            reject(new Error("Failed to fetch data"));
+          resolve(await res.json());
+        } catch (e) {
+          reject(e);
+        }
+      })
+      .catch((e) => reject(e));
+  });
+}
+
+export { getQuery, UUID, fetcher_json };
